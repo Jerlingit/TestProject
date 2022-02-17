58350505593b4a0c2707d7b1c500d674947ba5cb
Jerlingit <jerlinthangam24@gmail.com>
2022-02-17 15:29:06 +0530

    jerlin

---
+++ src/test/java/org/test/TestProject/Test1.java
@@ -0,0 +1,27 @@
+package org.test.TestProject;
+
+public class Test1 {
+	
+	public static void main(String[] args) {
+	
+	
+
+}
+
+	public static void test1() {	
+		System.out.println("Test1 completed");
+	}
+	
+	public static void test2() {	
+		System.out.println("Test2 completed");
+	}
+	
+	public static void test3() {	
+		System.out.println("Test3 completed");
+	}
+	
+	
+	public static void test4() {	
+		System.out.println("Test4 completed");
+	}
+}
